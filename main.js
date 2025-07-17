9189abfbedec3300c33313f8e7fcf5f69a60c4fc
Dardevs <rashidatadebanjo2009@gmail.com>
2025-07-17 12:59:55 +0100

    java

---
+++ main.js
@@ -0,0 +1,113 @@
+function number(zero) {
+   if(number > 0){
+       return "positive"
+   }else if(number === 0){
+      return 0
+   } else{
+       return "negative"
+   }
+}
+    
+undefined
+ 
+function check(hour) {
+   if(hour < 12){
+       return "good morning"
+   }else if(hour <= 17){
+      return "good afternoon"
+   } else{
+       return "good evening"
+   }
+   }
+    
+undefined
+function check(grade) {
+    if(grade >=90){
+        return "excellent"
+    }else if(grade >=70){
+        return "good"
+    }else if(grade >=50){
+    return "pass"
+    }else{
+        return "fail"
+    }
+    
+    
+}
+undefined
+function check(weather) {   
+if(weather === rainy){
+    return "take an umberalla"
+}else if(weather === "sunny"){
+    return "wear sunscreen"
+}else if(weather === "cold"){
+    return "pass"
+}else{
+    return "unknown"
+}
+}
+undefined
+{
+     if (age < 13){
+     return "child"
+     }else if(age <=19){
+         return "teenager"
+     }else if(age <=64){
+         return "adult"
+     }else{
+         return "senior"
+     }
+    
+}
+undefined
+function check(day) {
+    if(day === "saturday" || day === "sunday"){
+        return "its the weather"
+    }else{
+        return "its the weekday"
+    }
+    
+}
+undefined
+
+
+function traffic(light) {
+    if(light === "green"){
+    return "go"
+    }else if(light === "yellow"){   
+        return "slow down"
+    }else if(light === "red"){
+        return "stop"
+    }else{
+        return "unknown signal"
+    }
+    
+}
+undefined
+function name(program) {
+    if(program === "admin"){
+        return "welcome admin"
+    }else if(program === "editor"){
+        return "welcome editor"
+    }else if(program === "viewer"){
+        return "welcome viewer"
+    }else{
+        return "role not recognized"
+    }
+    
+}
+undefined
+function student(attendance) {
+    if(attendance === 100){
+        return "perfect attendance"
+    }else if(attendance >=75){
+        return "good attendance"
+    }else{
+        return "low attendance"
+    }
+    
+}
+
+undefined
+student(45)
+'low attendance'
